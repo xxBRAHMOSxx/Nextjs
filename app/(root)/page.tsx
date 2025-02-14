9fe0f5194ebaa4238e1b2526d3cdc5b31a5e5fc5
Devansh Kumar <devanshkumar6298@gmail.com>
2025-02-14 20:03:19 +0530

    enhance home page layout with new content; add utility functions for class management; update Tailwind CSS configuration and dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   console.log("Hello, World!");
   return (
-    <>
-    <h1 className="heading">home page</h1>
+    <> 
+        <section className="pink_container">
+          <h1 className="heading">pitch your startup,<br/> connect with enterpreneurs</h1>
+          <p className="sub-heading">
+            Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
+          </p>
+        </section>
+
     
     </>
   );
